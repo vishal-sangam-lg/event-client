547f13a85f5f9c77e8c1164d7c64550f402c0c57
Vishal Sangam L G <vishalsangamlg@Vishals-MacBook-Pro.local>
2023-02-16 17:50:06 +0530

    Form updated

--- src/components/EventForm.jsx
+++ src/components/EventForm.jsx
@@ -3,58 +3,141 @@ import styles from "./EventForm.module.css";
 import addEvent from "../services/event";
 
 export default function EventForm() {
-    const[data, setData] = useState({
-        name: "",
-        poster: "",
-        tags: "",
-        category: "",
-        description: "",
-        location: "",
-        whocanjoin: "college",
-        limit: "yes",
-        contact: ""
-    })
-    const imgToBase64 = (img) => {
-        let reader = new FileReader();
-        reader.readAsDataURL(img);
-        reader.onload = () => {
-            // reader.result => base64 img
-            addEvent(data.name, reader.result, data.tags, data.category, data.description, data.location, data.whocanjoin, data.limit, data.contact);
-        };
+  const categories = [
+    "technical",
+    "non-tech",
+    "quiz",
+    "seminar",
+    "hackathon",
+    "workshop",
+    "business",
+    "club-events",
+  ];
+  const [data, setData] = useState({
+    name: "",
+    poster: "",
+    tags: "",
+    category: [],
+    description: "",
+    location: "",
+    date: "",
+    time: "",
+    whocanjoin: "college",
+    limit: "yes",
+    contact: "",
+  });
+  const categoryChangeHandler = (event) => {
+    let updatedCategories = [...data.category];
+    if (event.target.checked) {
+      updatedCategories = [...data.category, event.target.value];
+    } else {
+      updatedCategories.splice(data.category.indexOf(event.target.value), 1);
     }
-    const submitHandler = (e) => {
-        e.preventDefault();
-        imgToBase64(data.poster);
-    }
-    return(
-        <form className={styles.form} onSubmit={submitHandler}>
-            <label>Event name</label>
-            <input type="text" value={data.name} onChange={(e) => setData({...data, name: e.target.value})} />
-            <label>Event poster</label>
-            <input type="file" onChange={(e) => setData({...data, poster: e.target.files[0]})} />
-            <label>Tags</label>
-            <input type="text" value={data.tags} onChange={(e) => setData({...data, tags: e.target.value})} />
-            <label>Category</label>
-            <input type="text" value={data.category} onChange={(e) => setData({...data, category: e.target.value})} />
-            <label>Description</label>
-            <input type="text" value={data.description} onChange={(e) => setData({...data, description: e.target.value})} />
-            <label>Location</label>
-            <input type="text" value={data.location} onChange={(e) => setData({...data, location: e.target.value})} />
-            <label>Who can join</label>
-            <select onChange={(e) => setData({...data, whocanjoin: e.target.value})} value={data.whocanjoin}
-            >
-                <option value="college">College Only</option>
-                <option value="anyone">Anyone can join</option>
-            </select>
-            <label>Registration limit</label>
-            <select onChange={(e) => setData({...data, limit: e.target.value})} value={data.limit}
-            >
-                <option value="yes">yes</option>
-                <option value="no">no</option>
-            </select>
-            <label>Contact Info</label>
-            <input type="text" value={data.contact} onChange={(e) => setData({...data, contact: e.target.value})} />
-            <button type="submit">submit</button>
-        </form>
-    )
+    setData({ ...data, category: updatedCategories });
+  };
+  const imgToBase64 = (img) => {
+    let reader = new FileReader();
+    reader.readAsDataURL(img);
+    reader.onload = () => {
+      // reader.result => base64 img
+      addEvent(
+        data.name,
+        reader.result,
+        data.tags,
+        data.category,
+        data.description,
+        data.location,
+        data.date,
+        data.time,
+        data.whocanjoin,
+        data.limit,
+        data.contact
+      );
+    };
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    imgToBase64(data.poster);
+  };
+  return (
+    <form className={styles.form} onSubmit={submitHandler}>
+      <label>Event name</label>
+      <input
+        type="text"
+        value={data.name}
+        onChange={(e) => setData({ ...data, name: e.target.value })}
+      />
+      <label>Event poster</label>
+      <input
+        type="file"
+        onChange={(e) => setData({ ...data, poster: e.target.files[0] })}
+      />
+      <label>Tags</label>
+      <input
+        type="text"
+        value={data.tags}
+        onChange={(e) => setData({ ...data, tags: e.target.value })}
+      />
+      <label>Category (Max 3)</label>
+      {categories.map((item, i) => {
+        return (
+          <div key={i}>
+            <input
+              value={item}
+              type="checkbox"
+              onChange={categoryChangeHandler}
+            />
+            <span>{item}</span>
+          </div>
+        );
+      })}
+      <label>Description</label>
+      <input
+        type="text"
+        value={data.description}
+        onChange={(e) => setData({ ...data, description: e.target.value })}
+      />
+      <label>Location</label>
+      <input
+        type="text"
+        value={data.location}
+        onChange={(e) => setData({ ...data, location: e.target.value })}
+      />
+      <label>Date</label>
+      <input
+        type="date"
+        value={data.date}
+        onChange={(e) => setData({ ...data, date: e.target.value })}
+      />
+      <label>Time</label>
+      <input
+        type="time"
+        value={data.time}
+        onChange={(e) => setData({ ...data, time: e.target.value })}
+      />
+      <label>Who can join</label>
+      <select
+        onChange={(e) => setData({ ...data, whocanjoin: e.target.value })}
+        value={data.whocanjoin}
+      >
+        <option value="college">College Only</option>
+        <option value="anyone">Anyone can join</option>
+      </select>
+      <label>Registration limit</label>
+      <select
+        onChange={(e) => setData({ ...data, limit: e.target.value })}
+        value={data.limit}
+      >
+        <option value="yes">yes</option>
+        <option value="no">no</option>
+      </select>
+      <label>Contact Info</label>
+      <input
+        type="text"
+        value={data.contact}
+        onChange={(e) => setData({ ...data, contact: e.target.value })}
+      />
+      <button type="submit">submit</button>
+    </form>
+  );
 }
--- src/services/event.js
+++ src/services/event.js
@@ -1,15 +1,32 @@
 import axios from "axios";
 
-export default function addEvent(name, poster, tags, category, description, location, whocanjoin, limit, contact) {
-    axios.post("http://localhost:8000/add-event", {
-        name: name,
-        poster: poster,
-        tags: tags,
-        category: category,
-        description: description,
-        location: location,
-        whocanjoin: whocanjoin,
-        limit: limit,
-        contact: contact
-    }).then(res => console.log(res)).catch(err => console.log(err))
+export default function addEvent(
+  name,
+  poster,
+  tags,
+  category,
+  description,
+  location,
+  date,
+  time,
+  whocanjoin,
+  limit,
+  contact
+) {
+  axios
+    .post("http://localhost:8000/add-event", {
+      name: name,
+      poster: poster,
+      tags: tags,
+      category: category,
+      description: description,
+      location: location,
+      date: date,
+      time: time,
+      whocanjoin: whocanjoin,
+      limit: limit,
+      contact: contact,
+    })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
 }
